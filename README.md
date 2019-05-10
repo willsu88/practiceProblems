becd623d3cc0f99a4151bb58b7fef0ffc7bd6668
William Su <ts47@rice.edu>
2019-05-10 13:29:54 -0500

    may 10, 2019

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# practiceProblems
+#Practice Problems
+This is a repo where I organize and keep track of all 
+the practice problems I've done. I'll use this to study
+later on.
---
+++ reverseInteger.java
@@ -0,0 +1,33 @@
+
+//https://leetcode.com/problems/reverse-integer/submissions/
+/* Given a 32-bit signed integer, reverse digits of an integer.
+ * Assume we are dealing with an environment which could only store integers 
+ * within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose 
+ * of this problem, assume that your function returns 0 when the reversed integer 
+ * overflows.
+ * Date: May 10, 2019
+ */
+
+class ReverseInteger {
+    public int reverse(int x) {
+    
+        int result = 0;
+        int curr = x; // Don't modify input.
+        while (curr !=0) {
+            int digit = curr % 10; //Pop digit.
+            curr = curr / 10;      //Get next iteration.
+            
+            /* Check for overflows.*/
+            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && digit > 7)) 
+                return 0; //(2^31 - 1 = 2147483647) that's where the 7 comes from.
+            if (result < Integer.MIN_VALUE/10 || (result == Integer.MIN_VALUE/10 && digit < -8)) 
+                return 0;  //(-2^31= -2147483648) that's where the -8 comes from.
+            
+            /* Add digit to our result. */
+            result = (result * 10) + digit;
+        }
+        
+        return result;
+        
+    }
+}
